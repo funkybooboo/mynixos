03cc6649c31bea7ccdc3404fb537737d5565d2db
Nate Stott <nate.stott@pm.me>
2024-12-01 15:04:30 -0700

    6 current   2024-12-01 15:04:28  24.11.710050.62c435d93bf0  6.6.63                          *

--- debbie.nix
+++ debbie.nix
@@ -198,7 +198,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11";
+  system.stateVersion = "unstable";
 
   system.autoUpgrade = {
     enable = true;
